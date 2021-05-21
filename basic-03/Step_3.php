e9f2d66a5f3cacc9e1721e9ee4f94cb700d2b0ac
Maria <mariamoussamoussa96@gmail.com>
2021-05-21 21:08:31 +0300

    step 3 done

---
+++ basic-03/Step_3.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Maria";
+$email = "mariamoussamoussa96@gmail.com";
+echo "My name is $name and my email is $email <br>";
+
+$height = 50;
+$width = 10;
+$area= $height * $width;
+echo "Area: $area";
+
+?>
