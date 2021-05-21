ffe85b075092ed6a50ef156eea771af349738e4e
Maria <mariamoussamoussa96@gmail.com>
2021-05-21 21:36:42 +0300

    step 4 done

---
+++ basic-04/Step_04.php
@@ -0,0 +1,8 @@
+<?php
+$name="Maria";
+$position="web developer";
+$github_url="https://github.com/mariamoussa";
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url <br><br>";
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url."<br><br>";
+echo 'Hello, I\'m '.$name.', I\'m a '.$position. '. please check my github link '.$github_url;
+?>
