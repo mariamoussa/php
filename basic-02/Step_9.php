4e11787eaec74f28739072c88149f0c48b3f3df5
Maria <mariamoussamoussa96@gmail.com>
2021-05-24 12:13:49 +0300

    Step 9 done

---
+++ basic-02/Step_9.php
@@ -0,0 +1,15 @@
+<?php
+function my_sqrt($n)
+{
+  $x = $n;
+  $y = 1;
+  while($x > $y)
+  {
+    $x = ($x + $y)/2;
+    $y = $n/$x;
+  }
+  return $x;
+}
+print_r(my_sqrt(16)."<br>");
+print_r(my_sqrt(14));
+?>
