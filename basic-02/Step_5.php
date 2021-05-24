99c8d8daf27485d6d8a43afbd8b8d2e1b537625b
Maria <mariamoussamoussa96@gmail.com>
2021-05-24 12:09:06 +0300

    Step 5 done

---
+++ basic-02/Step_5.php
@@ -0,0 +1,46 @@
+<?php
+
+function word_digit($word) {
+    // divide the string to an array
+$warr = explode(";",$word);
+$result = "";
+foreach($warr as $value){
+switch(trim($value)){
+case "zero":
+$result .= "0";
+break;
+case "one":
+$result .= "1";
+break;
+case "two":
+$result .= "2";
+break;
+case "three":
+$result .= "3";
+break;
+case "four":
+$result .= "4";
+break;
+case "five":
+$result .= "5";
+break;
+case "six":
+$result .= "6";
+break;
+case "seven":
+$result .= "7";
+break;
+case "eight":
+$result .= "8";
+break;
+case "nine":
+$result .= "9";
+break;
+}
+}
+return $result;
+}
+
+echo word_digit(five);
+
+?>
