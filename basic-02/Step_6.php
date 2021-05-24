b884716e4650acd6a45a6e88245da970f90ce654
Maria <mariamoussamoussa96@gmail.com>
2021-05-24 12:09:37 +0300

    Step 6 done

---
+++ basic-02/Step_6.php
@@ -0,0 +1,15 @@
+<?php
+
+function sum_of_digits($nums) {
+    $digits_sum = 0;
+      for ($i = 0; $i < strlen($nums); $i++) {
+             $digits_sum += $nums[$i];
+               }
+      return $digits_sum;
+}
+
+echo sum_of_digits("123456") ;
+echo "<br>";
+echo sum_of_digits("9999");
+
+?>
