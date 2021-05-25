05e20c94f1159505a2adea5f0c9381989c2178e0
Maria <mariamoussamoussa96@gmail.com>
2021-05-25 13:53:34 +0300

    Step 5 done

--- basic-03/Step_5.php
+++ basic-03/Step_5.php
@@ -1,3 +1,23 @@
 <?php
+//rectangle input
+// $e1 = 3; 
+// $e2 = 4;
+// $di = 5;
 
+//rhombus input
+$e1 = 5; 
+$e2 = 5;
+$di = 8;
+$rec = 0;
+$dia = 0;   
+    if ($e1 * $e1 + $e2 * $e2 === $di * $di) {
+        $rec++;
+    }
+    if ($e1 === $e2) {
+        $dia++;
+    } 
+if ($rec>0)
+echo "This is a rectangle";
+if ($dia>0)
+echo "This is a rhombus";
 ?>
