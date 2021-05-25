420f092538397b7ec4d737543bf85de9c8b7b131
Maria <mariamoussamoussa96@gmail.com>
2021-05-25 14:46:11 +0300

    Step 7 done

--- basic-03/Step_7.php
+++ basic-03/Step_7.php
@@ -1,14 +1,12 @@
-
-<?php
-for ($row=0; $row<7; $row++)
-{
-  for ($column=0; $column<=7; $column++)
-	{
-     if ((($row == 0 or $row == 6) and $column >= 0 and $column <= 6) or $row+$column==6)
-            echo "*";    
-        else  
-            echo " ";     
-	}        
-  echo "<br>";
-}
-?>
+ 
+ <?php
+    for ($i = 0; $i < 7; $i++) {
+        for ($j = 7; $j >=0; $j--) {
+            if ((($i == 0 or $i == 6) and $j >= 0 and $j <= 6) or $i + $j == 6)
+                echo "*";
+            else
+                echo "&nbsp;&nbsp;";
+        }
+        echo "<br/>";
+    }
+    ?>
